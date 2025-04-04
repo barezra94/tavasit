6154df5c27acafe1260fb44e04ea0b59f5a4b9bc
Bar Ezra Gafniel <bar@ivix.ai>
2025-04-04 17:16:48 +0300

    another try at building

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,10 @@
-"use client"
-
-import { TavasitCalculator } from "./(tavasitCalculator)/page";
+import  TavasitCalculator from "./(tavasitCalculator)/page";
 
 
 export default async function Page() {
 
     return (
       <>
-       
         <div>
           <TavasitCalculator/>
         </div>
